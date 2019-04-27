8a3d5b2c04aa6636ce363a651c65a0f15abf22c2
Chris Mackey <chris@mackeyarchitecture.com>
2019-04-27 13:00:21 -0400

    Update __init__.py
    Now Ladybug will load up neighboring plugins and load in the data collections correctly

--- ladybug/__init__.py
+++ ladybug/__init__.py
@@ -1,5 +1,24 @@
 """Ladybug library for Grasshopper."""
+# These must be imported here in this order because of how they reference eachother.
+import ladybug._datacollectionbase
+import ladybug.datacollection
+import ladybug.datacollectionimmutable
+
 import sys
+import importlib
+import pkgutil
+
+#  find and import ladybug plugins
+#  this is a critical step to add additional functionalities to ladybug core library.
+lb_plugins = {
+    name: importlib.import_module(name)
+    for finder, name, ispkg
+    in pkgutil.iter_modules()
+    if name.startswith('ladybug_')
+}
+
+for key in lb_plugins:
+    print('Successfully imported Ladybug plugin: {}'.format(key))
 
 # This is a variable to check if the library is a [+] library.
 setattr(sys.modules[__name__], 'isplus', True)
