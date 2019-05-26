77cc10bc2fa25e276de762eeeb49ab8022826241
Chris Mackey <chris@mackeyarchitecture.com>
2019-05-26 15:13:41 -0700

    Update toladybug.py

--- ladybug_rhino/toladybug.py
+++ ladybug_rhino/toladybug.py
@@ -1,8 +1,18 @@
 """Functions to create ladybug geometries from Rhino geometries."""
 
 try:
-    import ladybug_geometry.geometry2d as lb2
-    import ladybug_geometry.geometry3d as lb3
+    from ladybug_geometry.geometry2d.pointvector import Vector2D, Point2D
+    from ladybug_geometry.geometry2d.ray import Ray2D
+    from ladybug_geometry.geometry2d.line import LineSegment2D
+    from ladybug_geometry.geometry2d.polygon import Polygon2D
+    from ladybug_geometry.geometry2d.mesh import Mesh2D
+    from ladybug_geometry.geometry3d.pointvector import Vector3D, Point3D
+    from ladybug_geometry.geometry3d.ray import Ray3D
+    from ladybug_geometry.geometry3d.line import LineSegment3D
+    from ladybug_geometry.geometry3d.plane import Plane
+    from ladybug_geometry.geometry3d.mesh import Mesh3D
+    from ladybug_geometry.geometry3d.face import Face3D
+    from ladybug_geometry.geometry3d.polyface import Polyface3D
 except ImportError as e:
     raise ImportError(
         "Failed to import ladybug_geometry.\n{}".format(e))
@@ -25,22 +35,22 @@ except ImportError as e:
 
 def to_vector2d(vector):
     """Ladybug Vector2D from Rhino Vector3d."""
-    return lb2.pointvector.Vector2D(vector.X, vector.Y)
+    return Vector2D(vector.X, vector.Y)
 
 
 def to_point2d(point):
     """Ladybug Point2D from Rhino Point3d."""
-    return lb2.pointvector.Point2D(point.X, point.Y)
+    return Point2D(point.X, point.Y)
 
 
 def to_ray2d(ray):
     """Ladybug Ray2D from Rhino Ray3d."""
-    return lb2.ray.Ray2D(to_point2d(ray.Position), to_vector2d(ray.Direction))
+    return Ray2D(to_point2d(ray.Position), to_vector2d(ray.Direction))
 
 
 def to_linesegment2d(line):
     """Ladybug LineSegment2D from Rhino LineCurve."""
-    return lb2.line.LineSegment2D(
+    return LineSegment2D.from_end_points(
         to_point2d(line.PointAtStart), to_point2d(line.PointAtEnd))
 
 
@@ -48,7 +58,7 @@ def to_polygon2d(polygon):
     """Ladybug Polygon2D from Rhino closed PolyLineCurve."""
     assert polygon.IsClosed, \
         'Rhino PolyLineCurve must be closed to make a Ladybug Polygon2D.'
-    return lb2.polygon.Polygon2D(
+    return Polygon2D(
         [to_point2d(polygon.Point(i)) for i in range(polygon.PointCount - 1)])
 
 
@@ -56,7 +66,7 @@ def to_mesh2d(mesh, color_by_face=True):
     """Ladybug Mesh2D from Rhino Mesh."""
     lb_verts = tuple(to_point2d(pt) for pt in mesh.Vertices)
     lb_faces, colors = _extract_mesh_faces_colors(mesh, color_by_face)
-    return lb2.mesh.Mesh2D(lb_verts, lb_faces, colors)
+    return Mesh2D(lb_verts, lb_faces, colors)
 
 
 """____________3D GEOMETRY TRANSLATORS____________"""
@@ -64,28 +74,28 @@ def to_mesh2d(mesh, color_by_face=True):
 
 def to_vector3d(vector):
     """Ladybug Vector3D from Rhino Vector3d."""
-    return lb3.pointvector.Vector3D(vector.X, vector.Y, vector.Z)
+    return Vector3D(vector.X, vector.Y, vector.Z)
 
 
 def to_point3d(point):
     """Ladybug Point3D from Rhino Point3d."""
-    return lb3.pointvector.Point3D(point.X, point.Y, point.Z)
+    return Point3D(point.X, point.Y, point.Z)
 
 
 def to_ray3d(ray):
     """Ladybug Ray3D from Rhino Ray3d."""
-    return lb3.ray.Ray3D(to_point3d(ray.Position), to_vector3d(ray.Direction))
+    return Ray3D(to_point3d(ray.Position), to_vector3d(ray.Direction))
 
 
 def to_linesegment3d(line):
     """Ladybug LineSegment3D from Rhino LineCurve."""
-    return lb3.line.LineSegment3D(
+    return LineSegment3D(
         to_point3d(line.PointAtStart), to_point3d(line.PointAtEnd))
 
 
 def to_plane(pl):
     """Ladybug Plane from Rhino Plane."""
-    return lb3.plane.Plane(
+    return Plane(
         to_vector3d(pl.ZAxis), to_point3d(pl.Origin), to_vector3d(pl.XAxis))
 
 
@@ -93,7 +103,7 @@ def to_mesh3d(mesh, color_by_face=True):
     """Ladybug Mesh3D from Rhino Mesh."""
     lb_verts = tuple(to_point3d(pt) for pt in mesh.Vertices)
     lb_faces, colors = _extract_mesh_faces_colors(mesh, color_by_face)
-    return lb3.mesh.Mesh3D(lb_verts, lb_faces, colors)
+    return Mesh3D(lb_verts, lb_faces, colors)
 
 
 def to_face3d(brep, meshing_parameters=None):
@@ -119,8 +129,15 @@ def to_face3d(brep, meshing_parameters=None):
                     if f_norm.Z < 0:
                         loop_pcrv.Reverse()
                     loop_verts = []
-                    for i in range(loop_pcrv.SegmentCount):
-                        seg = loop_pcrv.SegmentCurve(i)
+                    try:
+                        loop_pcrvs = [loop_pcrv.SegmentCurve(i)
+                                      for i in range(loop_pcrv.SegmentCount)]
+                    except Exception:
+                        try:
+                            loop_pcrvs = [loop_pcrv[0]]
+                        except Exception:
+                            loop_pcrvs = [loop_pcrv]
+                    for seg in loop_pcrvs:
                         if seg.Degree == 1:
                             loop_verts.append(to_point3d(seg.PointAtStart))
                         else:
@@ -135,31 +152,32 @@ def to_face3d(brep, meshing_parameters=None):
                     all_verts.append([to_point3d(loop_pline.Item[i])
                                       for i in range(loop_pline.Count - 1)])
             if len(all_verts) == 1:  # No holes in the shape
-                faces.append(lb3.face.Face3D.from_vertices(all_verts[0]))
+                faces.append(Face3D.from_vertices(all_verts[0]))
             else:  # There's at least one hole in the shape
                 faces.append(
-                    lb3.face.Face3D.from_shape_with_holes(all_verts[0], all_verts[1:]))
+                    Face3D.from_shape_with_holes(all_verts[0], all_verts[1:]))
         else:
             if b_face.OrientationIsReversed:
                 b_face.Reverse(0, True)
-            meshed_brep = rg.Mesh.CreateFromBrep(b_face.ToBrep(), meshing_parameters)[0]
+            face_brep = b_face.DuplicateFace(True)
+            meshed_brep = rg.Mesh.CreateFromBrep(face_brep, meshing_parameters)[0]
             for m_face in meshed_brep.Faces:
                 if m_face.IsQuad:
-                    lb_face = lb3.face.Face3D.from_vertices(
+                    lb_face = Face3D.from_vertices(
                         tuple(to_point3d(meshed_brep.Vertices[i]) for i in
                               (m_face.A, m_face.B, m_face.C, m_face.D)))
                     if lb_face.validate_planarity(tolerance, False):
                         faces.append(lb_face)
                     else:
-                        lb_face_1 = lb3.face.Face3D.from_vertices(
+                        lb_face_1 = Face3D.from_vertices(
                             tuple(to_point3d(meshed_brep.Vertices[i]) for i in
                                   (m_face.A, m_face.B, m_face.C)))
-                        lb_face_2 = lb3.face.Face3D.from_vertices(
+                        lb_face_2 = Face3D.from_vertices(
                             tuple(to_point3d(meshed_brep.Vertices[i]) for i in
                                   (m_face.C, m_face.D, m_face.A)))
                         faces.extend([lb_face_1, lb_face_2])
                 else:
-                    lb_face = lb3.face.Face3D.from_vertices(
+                    lb_face = Face3D.from_vertices(
                         tuple(to_point3d(meshed_brep.Vertices[i]) for i in
                               (m_face.A, m_face.B, m_face.C)))
                     faces.append(lb_face)
@@ -176,7 +194,7 @@ def to_polyface3d(brep, meshing_parameters=None):
             Rhino's Default Meshing Parameters will be used.
     """
     lb_faces = to_face3d(brep)
-    return lb3.polyface.Polyface3D.from_faces_tolerance(lb_faces, tolerance)
+    return Polyface3D.from_faces_tolerance(lb_faces, tolerance)
 
 
 def _extract_mesh_faces_colors(mesh, color_by_face):
