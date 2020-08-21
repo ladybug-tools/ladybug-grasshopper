5ebf9d4793d4e8fa5692d379a162286182fb97bc
Chris <chris@ladybug.tools>
2020-08-21 17:27:31 -0400

    fix(version): Include .gha files with the installation

--- ladybug_grasshopper/__init__.py
+++ ladybug_grasshopper/__init__.py
@@ -1,6 +1,6 @@
 """Ladybug Grasshopper Plugin.
 
-Note that this package is not interned to run with cPython and it only possesses
+Note that this package is not intended to run with cPython and it only possesses
 the Grasshopper components. In order to run the plugin, the core libraries must
 be installed in a manner that they can be discovered by Rhino.
 The package includes both the userobjects (.ghuser) and the Python source (.py).
