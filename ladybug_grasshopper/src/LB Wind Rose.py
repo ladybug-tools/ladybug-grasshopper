5a2276ab843fa288a93a029d23f6ba8a27cf90bc
Saeran Vasanthakumar <saeranv@gmail.com>
2020-11-04 21:46:04 -0500

    feat(visualize): Update wind rose with frequency visualization
    * feat(LB Wind Rose): Update with different frequencies.
    
    Changes to averages and frequencies. Add polygon outlines to windrose mesh.
    
    * feat(LB Wind Rose): Revise frequencies, averages.
    
    Correct version number, and correct user object export.
    
    * feat(LB Wind Rose): Add angles and histogram_data.
    
    * feat(LB Wind Rose): Remove mean, frequencies. Objectify histogram.

--- ladybug_grasshopper/src/LB Wind Rose.py
+++ ladybug_grasshopper/src/LB Wind Rose.py
@@ -87,20 +87,21 @@ Create a plot of any hourly data by wind directions.
             rose directions.
         freq_line: Polygon geometries representing the frequency intervals of the wind
             rose.
+        windrose_line: Polygon geometries representing the windrose outlines.
         legend: Geometry representing the legend for the wind rose.
         title: A text object for the global_title.
         prevailing: The predominant direction of the outpt wind rose in clockwise
             degrees from north. 0 is North, 90 is East, 180 is South, 270 is West.
-        frequency: A list of integers for the number of values associated with
-            each wind direction.
-        avg_val: A list of average _data values with one value for each wind direction.
-        data: The input _data after it has gone through any of the statement or
-            period operations input to this component.
+        angles: A list of angles corresponding to each windrose directions.
+        calm_hours: The number of hours with calm wind speeds. Only returns a value if the input 
+            _data is wind speed. 
+        histogram_data: The input _data in a histogram structure after it has gone through any of 
+            the statement or period operations input to this component.
 """
 
 ghenv.Component.Name = 'LB Wind Rose'
 ghenv.Component.NickName = 'WindRose'
-ghenv.Component.Message = '1.0.0'
+ghenv.Component.Message = '1.0.1'
 ghenv.Component.Category = 'Ladybug'
 ghenv.Component.SubCategory = '2 :: Visualize Data'
 ghenv.Component.AdditionalHelpFromDocStrings = '2'
@@ -128,7 +129,7 @@ try:
     from ladybug_rhino.text import text_objects
     from ladybug_rhino.fromobjects import legend_objects, compass_objects
     from ladybug_rhino.grasshopper import \
-        all_required_inputs, list_to_data_tree, data_tree_to_list
+        all_required_inputs, list_to_data_tree, data_tree_to_list, objectify_output
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
 
@@ -154,21 +155,6 @@ if all_required_inputs(ghenv.Component):
             _data + [_wind_direction], statement_)
         _data = _fdata[:-1]
         _wind_direction = _fdata[-1]
-
-    # filter zero speed values out of collections if speed is input
-    pattern = []
-    filt_wind_dir = _wind_direction
-    for dat in _data:
-        if isinstance(dat.header.data_type, Speed):
-            for val in dat.values:
-                pat = True if val > 1e-10 else False
-                pattern.append(pat)
-            break
-    if len(pattern) != 0:
-        for i, dat in enumerate(_data):
-            if not isinstance(dat.header.data_type, Speed):
-                _data[i] = dat.filter_by_pattern(pattern)
-        filt_wind_dir = _wind_direction.filter_by_pattern(pattern)
 
     # set defaults and check errors in dir_count and north input
     if _dir_count_ is None:
@@ -204,20 +190,22 @@ if all_required_inputs(ghenv.Component):
     # set up empty lists of objects to be filled
     all_wind_avg_val = []
     all_wind_frequency = []
+    all_windrose_lines = []
     all_mesh = []
     all_compass = []
     all_orient_line = []
     all_freq_line = []
     all_legends = []
     all_title = []
-
+    all_calm_hours = []
+    all_histograms = []
+    
     # Calculate _max_freq_lines_ if it's not already set, to use to
     # determine spacing for multiple plots.
     if len(_data) > 1 and _max_freq_lines_ is None:
         max_freqs = []
         for i, _data_item in enumerate(_data):
-            win_dir = _wind_direction if isinstance(_data_item.header.data_type, Speed) \
-                else filt_wind_dir
+            win_dir = _wind_direction
             w = WindRose(win_dir, _data_item, _dir_count_)
             if _freq_hours_ is not None:
                 w.frequency_hours = _freq_hours_
@@ -225,13 +213,12 @@ if all_required_inputs(ghenv.Component):
                 w.frequency_spacing_distance = _freq_dist_
             max_freqs.append(w.frequency_intervals_compass)
         _max_freq_lines_ = max(max_freqs)
-
+    
     # Plot the windroses
     for i, speed_data in enumerate(_data):
 
         # Make the windrose
-        win_dir = _wind_direction if isinstance(speed_data.header.data_type, Speed) \
-            else filt_wind_dir
+        win_dir = _wind_direction
         windrose = WindRose(win_dir, speed_data, _dir_count_)
 
         if len(legend_par_) > 0:
@@ -253,56 +240,66 @@ if all_required_inputs(ghenv.Component):
 
         windrose.north = north_
         windrose.show_freq = _show_freq_
+        
+        calm_text = ''
         if isinstance(speed_data.header.data_type, Speed):
             windrose.show_zeros = _show_calmrose_
+            calm_text = '\nCalm for {}% of the time = {} hours.'.format(
+                round(windrose._zero_count / 
+                len(windrose.analysis_values) * 100.0, 2),
+                windrose._zero_count)
         windrose.base_point = Point2D(center_pt_2d.x, center_pt_2d.y)
-
+        
         # Make the mesh
         mesh = from_mesh2d(windrose.colored_mesh, _center_pt_.z)
-
+        
         # Make the graphic outputs
         legend = legend_objects(windrose.legend)
-        title = text_objects(title_text(speed_data),
+        freq_per = windrose._frequency_hours / \
+            len([b for a in windrose.histogram_data for b in a])
+        freq_text = '\nEach closed polyline shows frequency of {}% = {} hours.'.format(
+                round(freq_per * 100, 1), windrose._frequency_hours)
+        title = text_objects(title_text(speed_data) + calm_text + freq_text,
                              windrose.container.lower_title_location,
                              windrose.legend_parameters.text_height,
                              windrose.legend_parameters.font)
         compass = compass_objects(windrose.compass, _center_pt_.z, None)
         orient_line = [from_linesegment2d(seg, _center_pt_.z)
                             for seg in windrose.orientation_lines]
-        if _show_freq_:
-            freq_line = [from_polygon2d(poly) for poly in windrose.frequency_lines]
-
-        # move the center point so wind roses are not on top of one another
+        freq_line = [from_polygon2d(poly) for poly in windrose.frequency_lines]
+        windrose_lines = [from_polygon2d(poly) for poly in windrose.windrose_lines]
         fac = (i + 1) * windrose.compass_radius * 3
         center_pt_2d = Point2D(_center_pt_.x + fac, _center_pt_.y)
-
+        
+        # Add histogram 
+        hist_mtx = objectify_output('WindRose {}'.format(i), windrose.histogram_data)
+        
         all_mesh.append(mesh)
         all_compass.append(compass)
         all_orient_line.append(orient_line)
         all_freq_line.append(freq_line)
+        all_windrose_lines.append(windrose_lines)
         all_legends.append(legend)
         all_title.append(title)
+        calm = windrose.zero_count if isinstance(speed_data.header.data_type, Speed) else None
+        all_calm_hours.append(calm)
+        all_histograms.append(hist_mtx)
         
-        # compute the average values
-        wind_avg_val = []
-        for bin in windrose.histogram_data:
-            try:
-                wind_avg_val.append(sum(bin) / len(bin))
-            except ZeroDivisionError:
-                wind_avg_val.append(0)
-        all_wind_avg_val.append(wind_avg_val)
-        all_wind_frequency.append([len(bin) for bin in windrose.histogram_data])
-
     # convert nested lists into data trees
     mesh = list_to_data_tree(all_mesh)
     compass = list_to_data_tree(all_compass)
     orient_line = list_to_data_tree(all_orient_line)
     freq_line = list_to_data_tree(all_freq_line)
+    windrose_line = list_to_data_tree(all_windrose_lines)
     legend = list_to_data_tree(all_legends)
     title = list_to_data_tree(all_title)
-    avg_val = list_to_data_tree(all_wind_avg_val)
-    frequency = list_to_data_tree(all_wind_frequency)
+    calm_hours = list_to_data_tree(all_calm_hours)
+   
+    # Compute direction angles 
+    theta = 360.0 / windrose._number_of_directions
+    theta /= 2.0
+    angles = [(angle + theta) % 360.0 for angle in windrose.angles[:-1]]
 
     # output prevailing direction and processed data
     prevailing = windrose.prevailing_direction
-    data = _data
+    histogram = list_to_data_tree(all_histograms)
