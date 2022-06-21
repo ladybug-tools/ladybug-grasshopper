9a3bc1300477829106325b72d5bba69bd900949a
Chris Mackey <chris@ladybug.tools>
2022-06-21 11:41:22 -0400

    feat(extra): Add component to area aggregate

--- ladybug_grasshopper/src/LB Area Aggregate.py
+++ ladybug_grasshopper/src/LB Area Aggregate.py
@@ -8,27 +8,25 @@
 # @license AGPL-3.0-or-later <https://spdx.org/licenses/AGPL-3.0-or-later>
 
 """
-Get a Data Collection that is normalized by an area value.
+Get a Data Collection that is aggregated by an area value.
 _
 Note that this component will raise a ValueError if the data type in the header
-of the data collection is not normalizable to yeild a useful type. Also note
-that a ZeroDivisionError will be raised if the input area is equal to 0.
+of the data collection is not normalizable to yeild a useful type.
 -
 
     Args:
-        _data: A Data Collection to be normalized by the input _area.
-        _area: A number representing area by which all of the data is normalized.
+        _data: A Data Collection to be aggregated by the input _area.
+        _area: A number representing area by which all of the data is aggregated.
         _unit_: Text for the units that the area value is in. Acceptable inputs include
             'm2', 'ft2' and any other unit that is supported. (Default: m2).
 
     Returns:
-        data: A Ladybug data collection object derived that is normalized by the
-            input area.
+        data: A Ladybug data collection object aggregated by the input area
 """
 
-ghenv.Component.Name = 'LB Area Normalize'
-ghenv.Component.NickName = 'Normalize'
-ghenv.Component.Message = '1.5.1'
+ghenv.Component.Name = 'LB Area Aggregate'
+ghenv.Component.NickName = 'AreaAgg'
+ghenv.Component.Message = '1.5.0'
 ghenv.Component.Category = 'Ladybug'
 ghenv.Component.SubCategory = '4 :: Extra'
 ghenv.Component.AdditionalHelpFromDocStrings = '0'
@@ -41,4 +39,4 @@ except ImportError as e:
 
 if all_required_inputs(ghenv.Component):
     unit = _unit_ if _unit_ is not None else 'm2'
-    data = _data.normalize_by_area(_area, unit)
+    data = _data.aggregate_by_area(_area, unit)
